4277fa17eac404d09886d014c3156009ea89341e
Someone Someone <Anooxi72@gmail.com>
2020-11-06 11:37:53 +0100

    Ajout de routes + les exports associé

--- api/controllers/ProduitsController.js
+++ api/controllers/ProduitsController.js
@@ -1,7 +1,7 @@
 const Produit = require('../models/Produit');
 
 exports.getProduits = function (req, res) {
-    Produit.find({}, function (err, produits) {
+    Produit.find(req.query, function (err, produits) {
         if (err) return res.status(500).send(err);
         return res.status(200).json(produits);
     });
@@ -10,6 +10,31 @@ exports.getProduits = function (req, res) {
 exports.getCategories = function (req, res) {
     Produit.find({}, "categorie", function (err, produits) {
         if (err) return res.status(500).send(err);
-        return res.status(200).json(produits);
+        return res.status(200).json(produits.map(p => p.categorie));
     })
+}
+
+exports.addProduit = function (req, res) {
+    const newProduit = new Produit(req.body);
+    newProduit.save(function (err, produit) {
+        if (err) return res.status(500).send(err);
+        return res.status(201).json(produit);
+    })
+}
+
+exports.checkProduit = function (req, res, next, produitId) {
+    Produit.findOne({_id:produitId}, function (err,produit){
+        if(err) return res.status(500).send(err);
+        if(!produit) return res.status(404).json({
+            ok:false,
+            code:"PR40401",
+            message:"Le produit n'existe pas."
+        });
+        req.produit = produit;
+        return next();
+    })
+}
+
+exports.getProduit = function (req,res){
+    return res.status(200).json(req.produit);
 }
--- api/routes/produits/index.js
+++ api/routes/produits/index.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 const produitsController = require('../../controllers/ProduitsController');
 
 router.route("")
-    .get(produitsController.getProduits);
+    .get(produitsController.getProduits)
+    .post(produitsController.addProduit);
 
 router.route("/categories")
     .get(produitsController.getCategories);
+
+router.param("produitId",produitsController.checkProduit);
+router.route("/:produitId")
+    .get(produitsController.getProduit);
 
 module.exports = router;
