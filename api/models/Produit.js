eefd34001b1a4737ae7bca740115828bbbe55765
Charles <charles.desgenetez@gmail.com>
2020-12-04 09:05:06 +0100

    add description

--- api/models/Produit.js
+++ api/models/Produit.js
@@ -4,7 +4,8 @@ const produitSchema = new mongoose.Schema({
     nom: {type: String, required: true},
     type: {type: String, required: true},
     prix: {type: Number, required: true},
-    categorie: {type: String, required: true}
+    categorie: {type: String, required: true},
+    description: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Produits', produitSchema);
