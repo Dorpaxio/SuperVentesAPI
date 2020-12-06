5b9bb316f33c24356dee12932b6bfc33b516b135
Charles <charles.desgenetez@gmail.com>
2020-12-06 18:19:58 +0100

    add trie

--- api/controllers/ProduitsController.js
+++ api/controllers/ProduitsController.js
@@ -1,7 +1,15 @@
 const Produit = require('../models/Produit');
 
 exports.getProduits = function (req, res) {
-    Produit.find(req.query, function (err, produits) {
+    const {sort, ...where} = req.query;
+    const query = Produit.find(where);
+
+    if(sort) query.sort([sort.split(',')].map(arr => {
+        const [one, two] = arr[0].split('-');
+        return [two ? two : one, two ? -1 : 1];
+    }));
+
+    query.exec(function (err, produits) {
         if (err) return res.status(500).send(err);
         return res.status(200).json(produits);
     });
--- api/models/Produit.js
+++ api/models/Produit.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const produitSchema = new mongoose.Schema({
     nom: {type: String, required: true},
-    type: {type: String, required: true},
     prix: {type: Number, required: true},
     categorie: {type: String, required: true},
     description: {type: String, required: true}
